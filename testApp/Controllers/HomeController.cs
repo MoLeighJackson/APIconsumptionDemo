fb8fb7e8a8cd7ba6ef3b9f6141604a31d8e460de
MoLeighJackson <moleighjackson@gmail.com>
2017-08-17 08:44:06 -0400

    Initial upload
    Project name - testapp

---
+++ testApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using testApp.Models;
+
+namespace testApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult metroTransitInfo()
+        {
+            return View();
+        }
+
+        public ActionResult TransitInfo()
+        {
+            return View();
+        }
+
+        public JsonResult GetTransitInfo()
+        {
+            Transit tran = new Transit();
+            return Json(tran.getMetroTransitInfo(), JsonRequestBehavior.AllowGet);
+        }
+       
+    }
+}
---
+++ testApp/Models/Transit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Script.Serialization;
+
+// Alternat code using MVC to get API
+namespace testApp.Models
+{
+    public class Transit
+    {
+        public Object getMetroTransitInfo()
+        {
+            string url = "http://svc.metrotransit.org/NexTrip/Routes";
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var json = new JavaScriptSerializer().Serialize(content); // unsuccesful attempt to parse object array data
+            return json;
+
+        }
+    }
+}
